7329c0168533fdf5aa54693e77d1471b6ea16d14
yocastros <racasep@gmail.com>
2025-07-20 22:03:59 -0400

    función del while

--- rockpaperscissorsgame.py
+++ rockpaperscissorsgame.py
@@ -7,7 +7,7 @@ computer_wins = 0
 
 print("Let's play rock, paper, or scissors")
 
-while player_wins < 2 and computer_wins < 2:
+while player_wins < 2 and computer_wins < 2: # el juego termina cuando uno de los dos gana 2 veces
 
   player_choice = input("Please select rock, paper or scissors: ").lower()
 
